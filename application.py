979d3fdf3d391dbcb420c5457f2df2e71eceedc7
srujan2031 <chintasrujan07@gmail.com>
2025-07-15 01:26:47 -0500

    Deployment configuration AWS

--- application.py
+++ application.py
@@ -29,4 +29,4 @@ def predict_datapoint():
         return render_template('home.html', results=round(results[0], 2))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0")
